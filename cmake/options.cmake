73d6bc0023f012cfa2411db48faeef83e72e1eb7
William Dawson <William@wtdawson.info>
2026-06-02 23:08:26 +0100

    More CMake configuration

---
+++ cmake/options.cmake
@@ -0,0 +1,3 @@
+option(MSHTTP_BUILD         "Build MSHTTP"              ON )
+option(MSHTTP_BUILD_TEST    "Build the MSHTTP Test"     OFF)
+option(MSHTTP_BUILD_SHARED  "Build as shared"           OFF) # Defaults to static lib
